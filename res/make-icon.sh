6bd68380ce8a530e800e40b6aba3970a6f81483f
tildearrow <tildearrow@protonmail.com>
2021-12-29 17:50:10 -0500

    oh myyyyyyyy

--- res/make-icon.sh
+++ res/make-icon.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 sizes=(16 32 64 128 256 512)
-for i in $sizes; do
+if [ ! -e icon ]; then
+  mkdir icon
+fi
+for i in "${sizes[@]}"; do
   echo "making $i..."
   convert logo.png -filter Mitchell -scale "$i"x"$i" icon/icon_"$i"x"$i".png
 done
-for i in $sizes; do
+for i in "${sizes[@]}"; do
   echo "making $i@2x..."
   convert logo.png -filter Mitchell -scale "$((i*2))"x"$((i*2))" icon/icon_"$i"x"$i""@2x.png"
 done
