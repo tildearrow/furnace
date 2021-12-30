cdfe89565c119a3796f70cc46a2664f3af365382
tildearrow <tildearrow@protonmail.com>
2021-12-30 17:58:46 -0500

    improvements to AppImage release script

--- scripts/release-linux.sh
+++ scripts/release-linux.sh
@@ -28,6 +28,8 @@ cd ../release/linux/furnace.AppDir
 cp ../../../res/logo.png furnace.png || exit 1
 ln -s furnace.png .DirIcon || exit 1
 cp ../../../res/furnace.desktop . || exit 1
+mkdir -p usr/share/metainfo || exit 1
+cp ../../../res/furnace.appdata.xml usr/share/metainfo/org.tildearrow.furnace.metainfo.xml || exit 1
 cp ../../../res/AppRun . || exit 1
 
 cp /usr/lib/libm.so.6 usr/lib/ || exit 1
@@ -35,7 +37,14 @@ cp /usr/lib/libstdc++.so.6 usr/lib/ || exit 1
 cp /usr/lib/libc.so.6 usr/lib/ || exit 1
 cp /usr/lib/libgcc_s.so.1 usr/lib/ || exit 1
 
-cd ..
+cd usr/lib
+rm *.a || exit 1
+strip *.so.*
+
+cd ../bin
+strip -s furnace
+
+cd ../../..
 
 [ -e appimagetool-x86_64.AppImage ] || { wget "https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage" && chmod 755 appimagetool-x86_64.AppImage; }
 ARCH=$(uname -m) ./appimagetool-x86_64.AppImage furnace.AppDir
