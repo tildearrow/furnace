cfab60ad214211d45483555e1a363a1a8b167c3b
tildearrow <tildearrow@protonmail.com>
2022-01-06 18:38:58 -0500

    update iconset

--- res/make-icon.sh
+++ res/make-icon.sh
@@ -5,9 +5,9 @@ if [ ! -e icon.iconset ]; then
 fi
 for i in "${sizes[@]}"; do
   echo "making $i..."
-  convert logo.png -filter Mitchell -scale "$i"x"$i" icon/icon_"$i"x"$i".png
+  convert logo.png -filter Mitchell -scale "$i"x"$i" icon.iconset/icon_"$i"x"$i".png
 done
 for i in "${sizes[@]}"; do
   echo "making $i@2x..."
-  convert logo.png -filter Mitchell -scale "$((i*2))"x"$((i*2))" icon/icon_"$i"x"$i""@2x.png"
+  convert logo.png -filter Mitchell -scale "$((i*2))"x"$((i*2))" icon.iconset/icon_"$i"x"$i""@2x.png"
 done
