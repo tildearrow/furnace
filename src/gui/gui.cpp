9193ec0f14635bba6bec9542d3f345445b273373
tildearrow <tildearrow@protonmail.com>
2022-01-16 01:47:19 -0500

    add attribution for SAA1099 core

--- src/gui/gui.cpp
+++ src/gui/gui.cpp
@@ -1628,7 +1628,7 @@ void FurnaceGUI::drawPattern() {
   ImGui::End();
 }
 
-const char* aboutLine[54]={
+const char* aboutLine[55]={
   "tildearrow",
   "is proud to present",
   "",
@@ -1651,6 +1651,7 @@ const char* aboutLine[54]={
   "ymfm by Aaron Giles",
   "MAME SN76496 by Nicola Salmoria",
   "MAME AY-3-8910 by Couriersud",
+  "MAME SAA1099 by Juergen Buchmueller and Manuel Abadia",
   "SameBoy by Lior Halphon",
   "Mednafen PCE",
   "puNES by FHorse",
@@ -1734,7 +1735,7 @@ void FurnaceGUI::drawAbout() {
       }
     }
 
-    for (int i=0; i<54; i++) {
+    for (int i=0; i<55; i++) {
       double posX=(scrW*dpiScale/2.0)+(sin(double(i)*0.5+double(aboutScroll)/90.0)*120*dpiScale)-(ImGui::CalcTextSize(aboutLine[i]).x*0.5);
       double posY=(scrH-aboutScroll+42*i)*dpiScale;
       if (posY<-80*dpiScale || posY>scrH*dpiScale) continue;
@@ -1757,7 +1758,7 @@ void FurnaceGUI::drawAbout() {
     ImGui::PopFont();
     aboutScroll+=2;
     if (++aboutSin>=2400) aboutSin=0;
-    if (aboutScroll>(42*54+scrH)) aboutScroll=-20;
+    if (aboutScroll>(42*55+scrH)) aboutScroll=-20;
   }
   ImGui::End();
 }
