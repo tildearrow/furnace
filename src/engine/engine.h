f652d6c738a400fe2972605a0742ebc846216da0
tildearrow <tildearrow@protonmail.com>
2022-01-18 02:09:47 -0500

    fix devendor again and prepare for 0.4.2

--- src/engine/engine.h
+++ src/engine/engine.h
@@ -10,8 +10,8 @@
 #include <map>
 #include <queue>
 
-#define DIV_VERSION "0.4.1"
-#define DIV_ENGINE_VERSION 21
+#define DIV_VERSION "0.4.2"
+#define DIV_ENGINE_VERSION 22
 
 enum DivStatusView {
   DIV_STATUS_NOTHING=0,
