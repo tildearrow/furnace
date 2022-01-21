c8eeac4f633b9ceee539349be4a31188fb671d8d
tildearrow <tildearrow@protonmail.com>
2022-01-21 15:34:06 -0500

    update mac release script
    no longer pack the .zip, but got to figure out a way to insert
    files on the .dmg

--- scripts/release-mac.sh
+++ scripts/release-mac.sh
@@ -16,7 +16,7 @@ fi
 
 cd macbuild
 
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS="-O2" -DCMAKE_CXX_FLAGS="-O2 -Wall -Wextra -Wno-unused-parameter -Wno-unused-function -Werror" -DBUILD_PROGRAMS=OFF -DBUILD_EXAMPLES=OFF -DBUILD_TESTING=OFF -DENABLE_EXTERNAL_LIBS=OFF -DENABLE_MPEG=OFF .. || exit 1
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS="-O2" -DCMAKE_CXX_FLAGS="-O2 -Wall -Wextra -Wno-unused-parameter -Wno-unused-function -Werror" -DCMAKE_EXE_LINKER_FLAGS="" -DBUILD_PROGRAMS=OFF -DBUILD_EXAMPLES=OFF -DBUILD_TESTING=OFF -DENABLE_EXTERNAL_LIBS=OFF -DENABLE_MPEG=OFF .. || exit 1
 make -j4 || exit 1
 cpack || exit 1
 
@@ -26,8 +26,3 @@ mkdir -p release/macos || exit 1
 cd release/macos
 
 cp ../../macbuild/_CPack_Packages/Darwin/Bundle/Furnace*.dmg .
-cp ../../LICENSE LICENSE.txt || exit 1
-cp ../../README.md README.txt || exit 1
-cp -r ../../papers papers || exit 1
-
-zip -r furnace.zip LICENSE.txt Furnace*.dmg README.txt papers
