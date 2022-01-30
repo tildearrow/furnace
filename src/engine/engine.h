a1d3d3d87382edfce2237467294c49bab21bf8ac
tildearrow <tildearrow@protonmail.com>
2022-01-30 18:23:40 -0500

    release 0.5
    finally

--- src/engine/engine.h
+++ src/engine/engine.h
@@ -11,8 +11,8 @@
 #include <map>
 #include <queue>
 
-#define DIV_VERSION "0.5pre5"
-#define DIV_ENGINE_VERSION 34
+#define DIV_VERSION "0.5"
+#define DIV_ENGINE_VERSION 35
 
 enum DivStatusView {
   DIV_STATUS_NOTHING=0,
