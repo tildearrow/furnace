c83e146d11345fa303a5fa35840ce6acc504fb6e
Christoph Neidahl <christoph.neidahl@gmail.com>
2022-02-01 21:50:25 +0100

    Improve devendoring & other CMake stuff (#21)
    * Improve devendoring
    
    * Add warning flags for own C++ code
    
    For https://github.com/tildearrow/furnace/pull/12#issuecomment-1017330467.
    
    * Document new CMake options
    
    * pkg-config stuff
    
    * Warnings stuff for MSVC yay
    
    * Use more specific linking vars & functions
    
    * Fix Linux icon location
    
    * DEPENDENCIES_LEGACY_LDFLAGS for JACK too

--- src/audio/sdl.h
+++ src/audio/sdl.h
@@ -1,9 +1,5 @@
 #include "taAudio.h"
-#if (defined(_WIN32) || defined(HAVE_GUI)) && !defined(DEVENDOR_LIBRARIES)
 #include <SDL.h>
-#else
-#include <SDL2/SDL.h>
-#endif
 
 class TAAudioSDL: public TAAudio {
   SDL_AudioSpec ac, ar;
