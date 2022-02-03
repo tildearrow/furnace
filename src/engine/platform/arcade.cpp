35e39d2c32f0c989fe7387f14ab6e345c80cd8f7
tildearrow <tildearrow@protonmail.com>
2022-02-03 02:30:21 -0500

    arcade limits

--- src/engine/platform/arcade.cpp
+++ src/engine/platform/arcade.cpp
@@ -350,6 +350,8 @@ void DivPlatformArcade::tick() {
   for (int i=0; i<8; i++) {
     if (chan[i].freqChanged) {
       chan[i].freq=chan[i].baseFreq+(chan[i].pitch>>1)-64;
+      if (chan[i].freq<0) chan[i].freq=0;
+      if (chan[i].freq>=(95<<6)) chan[i].freq=(95<<6)-1;
       immWrite(i+0x28,hScale(chan[i].freq>>6));
       immWrite(i+0x30,chan[i].freq<<2);
       chan[i].freqChanged=false;
