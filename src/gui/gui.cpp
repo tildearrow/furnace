dc46c67d9f6ae259d2c08e9398b573f3fce39ab4
tildearrow <tildearrow@protonmail.com>
2022-02-10 21:14:27 -0500

    GUI: set debug window size constraints

--- src/gui/gui.cpp
+++ src/gui/gui.cpp
@@ -4026,6 +4026,7 @@ void FurnaceGUI::drawDebug() {
   static int bpTick;
   static bool bpOn;
   if (!debugOpen) return;
+  ImGui::SetNextWindowSizeConstraints(ImVec2(400.0f*dpiScale,200.0f*dpiScale),ImVec2(scrW*dpiScale,scrH*dpiScale));
   if (ImGui::Begin("Debug",&debugOpen,ImGuiWindowFlags_NoDocking)) {
     ImGui::Text("NOTE: use with caution.");
     if (ImGui::TreeNode("Debug Controls")) {
