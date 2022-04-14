2dc80e8d8ed859f1fa046b21b1faf498eed3940a
James Alan Nguyen <djtubig.malicex@gmail.com>
2022-04-15 01:07:11 +1000

    Batch file wasn't supposed to be there

--- buildme.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-git pull
-git submodule update --init --recursive
-cmake -Bbuild
-cmake --build build --target ALL_BUILD --config Release --parallel 12
-copy build\Release\furnace.exe \Apps\Audio\Furnace\
