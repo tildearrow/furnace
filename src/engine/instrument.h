tildearrow/furnace

commit 40ce6b56866734121c997e5b618647990a0cd04e

Change instrument type to official assigned number.

src/engine/instrument.h
@@ -54,7 +54,7 @@ enum DivInstrumentType: unsigned short {
   DIV_INS_VERA=24,
   DIV_INS_X1_010=25,
   DIV_INS_VRC6_SAW=26,
-  DIV_INS_MULTIPCM=27,  // TODO ask for official number assignment
+  DIV_INS_MULTIPCM=28,
   DIV_INS_MAX,
 };
 

commit dab6c913331f7e86c6c3a2f95cc3dd8a848f955e

Fix issue after changing instrument type number.
Broke it in 40ce6b5686.

src/engine/instrument.h
@@ -54,6 +54,7 @@ enum DivInstrumentType: unsigned short {
   DIV_INS_VERA=24,
   DIV_INS_X1_010=25,
   DIV_INS_VRC6_SAW=26,
+  DIV_INS_ES5506=27,
   DIV_INS_MULTIPCM=28,
   DIV_INS_MAX,
 };

src/gui/guiConst.cpp
@@ -107,6 +107,7 @@ const char* insTypes[DIV_INS_MAX]={
   "VERA",
   "X1-010",
   "VRC6 (saw)",
+  "ES5506",
   "MultiPCM/OPL4"
 };
 