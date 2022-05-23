82eaa45f5d366a78477f4dc6d7d2134daab7649e
tildearrow <tildearrow@protonmail.com>
2022-05-22 22:47:40 -0500

    increase range of relative pitch macro
    -32768 to 32767 should be enough

--- src/engine/platform/opl.cpp
+++ src/engine/platform/opl.cpp
@@ -384,7 +384,7 @@ void DivPlatformOPL::tick(bool sysTick) {
     if (chan[i].std.pitch.had) {
       if (chan[i].std.pitch.mode) {
         chan[i].pitch2+=chan[i].std.pitch.val;
-        CLAMP_VAR(chan[i].pitch2,-2048,2048);
+        CLAMP_VAR(chan[i].pitch2,-32768,32767);
       } else {
         chan[i].pitch2=chan[i].std.pitch.val;
       }
