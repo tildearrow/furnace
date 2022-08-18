3f14625361f2cc8e3bfae3e6e0095993fcc49da2
tildearrow <tildearrow@protonmail.com>
2022-08-18 18:05:45 -0500

    Revert "Fix initializing"
    This reverts commit 0b09408ee87cc3f2efa4b8908b6e8124ff1da33b.

--- src/gui/gui.h
+++ src/gui/gui.h
@@ -858,10 +858,6 @@ struct FurnaceGUISysCategory {
     description(NULL) {}
 };
 
-static const char* dummyMode[1]={
-  NULL
-};
-
 struct FurnaceGUIMacroDesc {
   DivInstrumentMacro* macro;
   int min, max;
@@ -874,7 +870,7 @@ struct FurnaceGUIMacroDesc {
   bool isBitfield, blockMode, useMacroMode;
   String (*hoverFunc)(int,float);
 
-  FurnaceGUIMacroDesc(const char* name, DivInstrumentMacro* m, int macroMin, int macroMax, float macroHeight, ImVec4 col=ImVec4(1.0f,1.0f,1.0f,1.0f), bool block=false, bool mMode=false, const char** mName=dummyMode, String (*hf)(int,float)=NULL, bool bitfield=false, const char** bfVal=NULL, unsigned int bitOff=0):
+  FurnaceGUIMacroDesc(const char* name, DivInstrumentMacro* m, int macroMin, int macroMax, float macroHeight, ImVec4 col=ImVec4(1.0f,1.0f,1.0f,1.0f), bool block=false, bool mMode=false, const char** mName={NULL}, String (*hf)(int,float)=NULL, bool bitfield=false, const char** bfVal=NULL, unsigned int bitOff=0):
     macro(m),
     height(macroHeight),
     displayName(name),
