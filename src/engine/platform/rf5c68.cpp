9c29b7e7cab951f25a76d9b8921872b353fb53db
cam900 <dbtlrchl@naver.com>
2022-08-25 00:24:49 +0900

    Fix RF5C68 panning macro

--- src/engine/platform/rf5c68.cpp
+++ src/engine/platform/rf5c68.cpp
@@ -107,7 +107,7 @@ void DivPlatformRF5C68::tick(bool sysTick) {
       chan[i].panning|=(chan[i].std.panR.val&15)<<4;
     }
     if (chan[i].std.panL.had || chan[i].std.panR.had) {
-      chWrite(i,0x05,isMuted[i]?0:chan[i].panning);
+      chWrite(i,1,isMuted[i]?0:chan[i].panning);
     }
     if (chan[i].setPos) {
       // force keyon
