897a61db85ccfc41fda22779dc2657f73a12bafb
tildearrow <tildearrow@protonmail.com>
2022-08-26 16:44:27 -0500

    copy paste

--- src/gui/sysManager.cpp
+++ src/gui/sysManager.cpp
@@ -42,6 +42,54 @@ void FurnaceGUI::drawSysManager() {
         showError(e->getLastError());
       }
     }
+
+    if (ImGui::BeginTable("SystemList",3)) {
+      ImGui::TableSetupColumn("c1",ImGuiTableColumnFlags_WidthFixed,0.0);
+      ImGui::TableSetupColumn("c2",ImGuiTableColumnFlags_WidthStretch,0.0);
+      ImGui::TableSetupColumn("c3",ImGuiTableColumnFlags_WidthFixed,48.0f*dpiScale);
+      ImGui::TableNextRow(ImGuiTableRowFlags_Headers);
+      ImGui::TableNextColumn();
+      ImGui::Text("Visible");
+      ImGui::TableNextColumn();
+      ImGui::Text("Name");
+      for (int i=0; i<e->getTotalChannelCount(); i++) {
+        ImGui::PushID(i);
+        ImGui::TableNextRow();
+        ImGui::TableNextColumn();
+        ImGui::Checkbox("##Visible",&e->curSubSong->chanShow[i]);
+        ImGui::SameLine();
+        if (ImGui::Button(ICON_FA_ARROWS)) {
+        }
+        if (ImGui::BeginDragDropSource()) {
+          chanToMove=i;
+          ImGui::SetDragDropPayload("FUR_CHAN",NULL,0,ImGuiCond_Once);
+          ImGui::Button(ICON_FA_ARROWS "##ChanDrag");
+          ImGui::EndDragDropSource();
+        } else if (ImGui::IsItemHovered()) {
+          ImGui::SetTooltip("%s #%d\n(drag to swap channels)",e->getSystemName(e->sysOfChan[i]),e->dispatchChanOfChan[i]);
+        }
+        if (ImGui::BeginDragDropTarget()) {
+          const ImGuiPayload* dragItem=ImGui::AcceptDragDropPayload("FUR_CHAN");
+          if (dragItem!=NULL) {
+            if (dragItem->IsDataType("FUR_CHAN")) {
+              if (chanToMove!=i && chanToMove>=0) {
+                e->swapChannelsP(chanToMove,i);
+              }
+              chanToMove=-1;
+            }
+          }
+          ImGui::EndDragDropTarget();
+        }
+        ImGui::TableNextColumn();
+        ImGui::SetNextItemWidth(ImGui::GetContentRegionAvail().x);
+        ImGui::InputTextWithHint("##ChanName",e->getChannelName(i),&e->curSubSong->chanName[i]);
+        ImGui::TableNextColumn();
+        ImGui::SetNextItemWidth(ImGui::GetContentRegionAvail().x);
+        ImGui::InputTextWithHint("##ChanShortName",e->getChannelShortName(i),&e->curSubSong->chanShortName[i]);
+        ImGui::PopID();
+      }
+      ImGui::EndTable();
+    }
   }
   if (ImGui::IsWindowFocused(ImGuiFocusedFlags_ChildWindows)) curWindow=GUI_WINDOW_SYS_MANAGER;
   ImGui::End();
