0c8cde9f85706bd46d5af43a6e16668486e75750
tildearrow <tildearrow@protonmail.com>
2022-09-09 00:02:43 -0500

    GUI: mark modified upon order val change

--- src/gui/gui.cpp
+++ src/gui/gui.cpp
@@ -1154,6 +1154,7 @@ void FurnaceGUI::keyDown(SDL_Event& ev) {
             e->lockSave([this,num]() {
               e->curOrders->ord[orderCursor][curOrder]=((e->curOrders->ord[orderCursor][curOrder]<<4)|num);
             });
+            MARK_MODIFIED;
             if (orderEditMode==2 || orderEditMode==3) {
               curNibble=!curNibble;
               if (!curNibble) {
