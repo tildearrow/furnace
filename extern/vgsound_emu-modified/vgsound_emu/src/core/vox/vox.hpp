eeb09c703147a0845c7eb4886dfe60caac23b45b
cam900 <dbtlrchl@naver.com>
2022-09-17 14:02:43 +0900

    Sync with master

--- extern/vgsound_emu-modified/vgsound_emu/src/core/vox/vox.hpp
+++ extern/vgsound_emu-modified/vgsound_emu/src/core/vox/vox.hpp
@@ -40,16 +40,10 @@ class vox_core : public vgsound_emu_core
 
 						s32 step() { return m_step; }
 
-						decoder_state_t &operator=(decoder_state_t &src)
-						{
-							copy_state(src);
-							return *this;
-						}
+						void copy_state(decoder_state_t &src);
 
 					private:
 						const bool m_wraparound = false;  // wraparound or clamp?
-
-						void copy_state(decoder_state_t &src);
 
 						vox_core &m_vox;
 						s8 m_index = 0;
@@ -76,7 +70,7 @@ class vox_core : public vgsound_emu_core
 				{
 					if (!m_loop_saved)
 					{
-						m_loop		 = m_curr;
+						m_loop.copy_state(m_curr);
 						m_loop_saved = true;
 					}
 				}
@@ -85,7 +79,7 @@ class vox_core : public vgsound_emu_core
 				{
 					if (m_loop_saved)
 					{
-						m_curr = m_loop;
+						m_curr.copy_state(m_loop);
 					}
 				}
 
