3f6ca8f650794b344e71b981594e11b917773961
tildearrow <tildearrow@protonmail.com>
2022-09-24 18:40:36 -0500

    fix build on my machine

--- src/engine/platform/sound/snes/SPC_DSP.cpp
+++ src/engine/platform/sound/snes/SPC_DSP.cpp
@@ -803,7 +803,7 @@ void SPC_DSP::run( int clocks_remain )
 	{
 	loop:
 		// GCC, why
-#ifdef __GNUC__
+#if defined(__GNUC__) && !defined(__clang__)
 		#define PHASE( n ) if ( n && !--clocks_remain ) break; __attribute__ ((fallthrough)); case n:
 #else
 		#define PHASE( n ) if ( n && !--clocks_remain ) break; case n:
