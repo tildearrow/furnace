c4be8c9f5da4b1d0be45a13b46f3817484e76820
tildearrow <tildearrow@protonmail.com>
2022-12-18 20:01:29 -0500

    let's try out mzpokeysnd
    to-do:
    - de-Atari800ize the core
    - remove resampling funcs
    - make Update* public
    -
    -
    - DivPlatformPOKEY already!

---
+++ src/engine/platform/sound/pokey/mzpokeysnd.h
@@ -0,0 +1,11 @@
+#ifndef MZPOKEYSND_H_
+#define MZPOKEYSND_H_
+
+int MZPOKEYSND_Init(size_t freq17,
+                        int playback_freq,
+                        int flags,
+                        int quality
+                        , int clear_regs
+                       );
+
+#endif /* MZPOKEYSND_H_ */
