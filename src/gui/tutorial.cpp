eba1191ef278398a53a38ccd129056899b122a24
tildearrow <tildearrow@protonmail.com>
2023-02-24 05:09:29 -0500

    1

--- src/gui/tutorial.cpp
+++ src/gui/tutorial.cpp
@@ -42,7 +42,46 @@ void FurnaceGUI::drawTutorial() {
     ImGui::Text("Welcome!");
     ImGui::PopFont();
 
-    ImGui::Text("Hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi");
+    ImGui::Text("welcome to Furnace, the biggest open-source chiptune tracker!");
+
+    ImGui::TextWrapped("this welcome screen is temporary. 0.6pre5 will feature a tutorial.");
+
+    ImGui::Separator();
+
+    ImGui::TextWrapped("here are some tips to get you started:");
+    
+    ImGui::TextWrapped(
+      "- add an instrument by clicking on + in Instruments\n"
+      "- click on the pattern view to focus it\n"
+      "- channel columns have the following, in this order: note, instrument, volume and effects\n"
+      "- hit space bar while on the pattern to toggle Edit Mode\n"
+      "- click on the pattern or use arrow keys to move the cursor\n"
+      "- values (instrument, volume, effects and effect values) are in hexadecimal\n"
+      "- hit enter to play/stop the song\n"
+      "- extend the song hy adding more orders in the Orders window\n"
+      "- click on the Orders matrix to change the patterns of a channel (left click increases; right click decreases)"
+    );
+
+    ImGui::TextWrapped(
+      "if you need help, you may:\n"
+      "- read the (incomplete) manual: https://github.com/tildearrow/furnace/blob/master/papers/doc/README.md\n"
+      "- ask for help in Discussions (https://github.com/tildearrow/furnace/discussions) or the Furnace Discord (https://discord.gg/EfrwT2wq7z)"
+    );
+
+    ImGui::Separator();
+
+    ImGui::TextWrapped("if you find any issues, be sure to report them! the issue tracker is here: https://github.com/tildearrow/furnace/issues");
+
+    ImGui::Separator();
+
+    ImGui::TextWrapped(
+      "there are two interface modes: Basic, and Advanced.\n"
+      "the Basic Mode only shows essential features. use it if you are new to trackers or prefer simplicity.\n"
+      "Advanced Mode allows you to use all Furnace features, but it may be confusing."
+    );
+
+    ImGui::TextWrapped("pick a mode to begin your journey! (you can always switch by going to Settings > Basic Mode)");
+
     if (ImGui::Button("Start in Basic Mode")) {
       basicMode=true;
       tutorial.protoWelcome=true;
