c7bff6b24e9fc0163f3c4f72133c6ed993deff7d
cam900 <dbtlrchl@naver.com>
2023-03-05 14:05:22 +0900

    Add preset for game.com and casio pv-1000

--- src/gui/presets.cpp
+++ src/gui/presets.cpp
@@ -256,6 +256,16 @@ void FurnaceGUI::initSystemPresets() {
       CH(DIV_SYSTEM_POKEMINI, 0.5f, 0, "")
     }
   );
+  ENTRY(
+    "Tiger Game.com", {
+      CH(DIV_SYSTEM_SM8521, 1.0f, 0, "")
+    }
+  );
+  ENTRY(
+    "Casio PV-1000", {
+      CH(DIV_SYSTEM_D65010G031, 1.0f, 0, "")
+    }
+  );
   CATEGORY_END;
 
   CATEGORY_BEGIN("Computers","let's get to work on chiptune today.");
