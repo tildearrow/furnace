982678eff31533b08f5f3264a125ef850397afc4
tildearrow <me@tildearrow.org>
2023-06-05 18:59:27 -0500

    GUI: fix one digit effect when it is 10xx
    issue #1141

--- src/gui/pattern.cpp
+++ src/gui/pattern.cpp
@@ -287,7 +287,7 @@ inline void FurnaceGUI::patternRow(int i, bool isPlaying, float lineHeight, int 
           if (pat->data[i][index]>0xff) {
             snprintf(id,63,"??##PE%d_%d_%d",k,i,j);
             ImGui::PushStyleColor(ImGuiCol_Text,uiColors[GUI_COLOR_PATTERN_EFFECT_INVALID]);
-          } else if (pat->data[i][index]>0x10 || settings.oneDigitEffects==0) {
+          } else if (pat->data[i][index]>=0x10 || settings.oneDigitEffects==0) {
             const unsigned char data=pat->data[i][index];
             snprintf(id,63,"%.2X##PE%d_%d_%d",data,k,i,j);
             ImGui::PushStyleColor(ImGuiCol_Text,uiColors[fxColors[data]]);
