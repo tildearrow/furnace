677f7fe682fc163f5aa001ce4b1cee03455a3cd6
tildearrow <me@tildearrow.org>
2023-07-26 19:16:57 -0500

    ES5506: some cleanup
    and fix sample map... ugh
    
    issue #1282

--- src/gui/insEdit.cpp
+++ src/gui/insEdit.cpp
@@ -5563,6 +5563,7 @@ void FurnaceGUI::drawInsEdit() {
           if (ins->type==DIV_INS_MSM6295) waveMax=0;
           if (ins->type==DIV_INS_SEGAPCM) waveMax=0;
           if (ins->type==DIV_INS_K007232) waveMax=0;
+          if (ins->type==DIV_INS_ES5506) waveMax=0;
           if (ins->type==DIV_INS_GA20) waveMax=0;
           if (ins->type==DIV_INS_K053260) waveMax=0;
           if (ins->type==DIV_INS_POKEMINI) waveMax=0;
