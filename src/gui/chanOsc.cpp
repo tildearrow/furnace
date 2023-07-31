98a70aee2a26cf2720d31f87a00b07a12fca91ba
Eknous-P <ehovakimyan053@gmail.com>
2023-07-31 20:50:41 +0400

    fix chan note text

--- src/gui/chanOsc.cpp
+++ src/gui/chanOsc.cpp
@@ -574,7 +574,10 @@ void FurnaceGUI::drawChanOsc() {
                       case 'n': {
                         DivChannelState* chanState=e->getChanState(ch);
                         if (chanState==NULL || !(chanState->keyOn)) break;
-                        text+=fmt::sprintf("%s",noteName(short (chanState->note),0));
+                        int tempNote=chanState->note;//all of this conversion is necessary because notes 100-102 are special chars
+                        int noteMod=tempNote%12+12;//also note 0 is a BUG, hence +12 on the note and -1 on the octave
+                        int oct=tempNote/12-1; 
+                        text+=fmt::sprintf("%s",noteName(short (noteMod),short (oct)));
                         break;
                       }
                       case '%':
