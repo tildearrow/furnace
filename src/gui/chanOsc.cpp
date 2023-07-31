ff1746462b5360186645fd5a17bb52159b598153
Eknous-P <ehovakimyan053@gmail.com>
2023-07-31 20:57:51 +0400

    change int to short

--- src/gui/chanOsc.cpp
+++ src/gui/chanOsc.cpp
@@ -574,10 +574,10 @@ void FurnaceGUI::drawChanOsc() {
                       case 'n': {
                         DivChannelState* chanState=e->getChanState(ch);
                         if (chanState==NULL || !(chanState->keyOn)) break;
-                        int tempNote=chanState->note;//all of this conversion is necessary because notes 100-102 are special chars
-                        int noteMod=tempNote%12+12;//also note 0 is a BUG, hence +12 on the note and -1 on the octave
-                        int oct=tempNote/12-1; 
-                        text+=fmt::sprintf("%s",noteName(short (noteMod),short (oct)));
+                        short tempNote=chanState->note;//all of this conversion is necessary because notes 100-102 are special chars
+                        short noteMod=tempNote%12+12;//also note 0 is a BUG, hence +12 on the note and -1 on the octave
+                        short oct=tempNote/12-1; 
+                        text+=fmt::sprintf("%s",noteName(noteMod,oct));
                         break;
                       }
                       case '%':
