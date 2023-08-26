962943185d010a2772f089d0609f6fe2199b6ae6
tildearrow <me@tildearrow.org>
2023-08-26 12:55:32 -0500

    split 32-bit Windows release

--- scripts/release-winxp.sh
+++ scripts/release-winxp.sh
@@ -8,23 +8,23 @@ fi
 
 cd /tmp/furnace
 
-if [ ! -e xpbuild ]; then
-  mkdir xpbuild || exit 1
+if [ ! -e win32build ]; then
+  mkdir win32build || exit 1
 fi
 
-cd xpbuild
+cd win32build
 
 # TODO: potential Arch-ism?
-i686-w64-mingw32-cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_C_FLAGS="-O2" -DCMAKE_CXX_FLAGS="-O2 -Wall -Wextra -Wno-unused-parameter -Wno-cast-function-type" -DBUILD_SHARED_LIBS=OFF -DSUPPORT_XP=OFF -DWITH_RENDER_DX11=ON .. || exit 1
+i686-w64-mingw32-cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_C_FLAGS="-O2" -DCMAKE_CXX_FLAGS="-O2 -Wall -Wextra -Wno-unused-parameter -Wno-cast-function-type" -DBUILD_SHARED_LIBS=OFF -DSUPPORT_XP=ON -DWITH_RENDER_DX11=OFF .. || exit 1
 make -j8 || exit 1
 
 cd ..
 
-mkdir -p release/winxp || exit 1
-cd release/winxp
+mkdir -p release/win32 || exit 1
+cd release/win32
 
 cp ../../LICENSE LICENSE.txt || exit 1
-cp ../../xpbuild/furnace.exe . || exit 1
+cp ../../win32build/furnace.exe . || exit 1
 cp ../../README.md README.txt || exit 1
 cp -r ../../papers papers || exit 1
 cp -r ../../doc doc || exit 1
@@ -34,8 +34,13 @@ cp -r ../../wavetables wavetables || exit 1
 
 i686-w64-mingw32-strip -s furnace.exe || exit 1
 
+# patch to remove GetTickCount64
+xxd -c 256 -ps furnace.exe | sed "s/4765745469636b436f756e743634/4765745469636b436f756e740000/g" | xxd -ps -r > furnace-patched.exe
+rm furnace.exe
+mv furnace-patched.exe furnace.exe
+
 zip -r furnace.zip LICENSE.txt furnace.exe README.txt papers doc demos instruments wavetables
 
 furName=$(git describe --tags | sed "s/v0/0/")
 
-mv furnace.zip furnace-"$furName"-win32-XP-ONLY.zip
+mv furnace.zip furnace-"$furName"-win32.zip
