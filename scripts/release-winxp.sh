bd92347950bf3358e75bb46ebff820b806ab79d2
tildearrow <me@tildearrow.org>
2023-08-26 13:09:01 -0500

    fix the release scripts

--- scripts/release-winxp.sh
+++ scripts/release-winxp.sh
@@ -8,11 +8,11 @@ fi
 
 cd /tmp/furnace
 
-if [ ! -e win32build ]; then
-  mkdir win32build || exit 1
+if [ ! -e xpbuild ]; then
+  mkdir xpbuild || exit 1
 fi
 
-cd win32build
+cd xpbuild
 
 # TODO: potential Arch-ism?
 i686-w64-mingw32-cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_C_FLAGS="-O2" -DCMAKE_CXX_FLAGS="-O2 -Wall -Wextra -Wno-unused-parameter -Wno-cast-function-type" -DBUILD_SHARED_LIBS=OFF -DSUPPORT_XP=ON -DWITH_RENDER_DX11=OFF .. || exit 1
@@ -20,11 +20,11 @@ make -j8 || exit 1
 
 cd ..
 
-mkdir -p release/win32 || exit 1
-cd release/win32
+mkdir -p release/winxp || exit 1
+cd release/winxp
 
 cp ../../LICENSE LICENSE.txt || exit 1
-cp ../../win32build/furnace.exe . || exit 1
+cp ../../xpbuild/furnace.exe . || exit 1
 cp ../../README.md README.txt || exit 1
 cp -r ../../papers papers || exit 1
 cp -r ../../doc doc || exit 1
@@ -43,4 +43,4 @@ zip -r furnace.zip LICENSE.txt furnace.exe README.txt papers doc demos instrumen
 
 furName=$(git describe --tags | sed "s/v0/0/")
 
-mv furnace.zip furnace-"$furName"-win32.zip
+mv furnace.zip furnace-"$furName"-win32-XP-ONLY.zip
