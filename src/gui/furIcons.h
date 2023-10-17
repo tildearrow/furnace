6ce2fd0c9a710633353670eda3fc53da621df00a
Kagamiin~ <kingchuxing.trills@aleeas.com>
2023-10-17 20:16:38 -0300

    Adding ESFM letter-style instrument icon
    Thanks to Electric Keet for providing me with the icon!

--- src/gui/furIcons.h
+++ src/gui/furIcons.h
@@ -1,7 +1,7 @@
 // not auto-generated. update every time you change icons.ttf!
 
 #define ICON_MIN_FUR 0xe0f0
-#define ICON_MAX_FUR 0xe142
+#define ICON_MAX_FUR 0xe143
 
 // test
 #define ICON_FUR_TEST0 u8"\ue0f0"
@@ -66,6 +66,7 @@
 #define ICON_FUR_INS_TED u8"\ue134"
 #define ICON_FUR_INS_C140 u8"\ue135"
 #define ICON_FUR_INS_C219 u8"\ue142"
+#define ICON_FUR_INS_ESFM u8"\ue143"
 
 // sample editor
 #define ICON_FUR_SAMPLE_APPLY_SILENCE u8"\ue136"
