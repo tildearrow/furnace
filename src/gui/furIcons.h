74d2e7e329a1919817d6bff7d16d63423bd3dc86
tildearrow <me@tildearrow.org>
2023-10-17 18:38:39 -0500

    aaaaaaa

--- src/gui/furIcons.h
+++ src/gui/furIcons.h
@@ -1,7 +1,7 @@
 // not auto-generated. update every time you change icons.ttf!
 
 #define ICON_MIN_FUR 0xe0f0
-#define ICON_MAX_FUR 0xe142
+#define ICON_MAX_FUR 0xe143
 
 // test
 #define ICON_FUR_TEST0 u8"\ue0f0"
