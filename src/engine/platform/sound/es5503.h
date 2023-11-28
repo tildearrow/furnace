674e706ca97447f44c35357170bacd2e0587dc03
LTVA1 <87536432+LTVA1@users.noreply.github.com>
2023-11-28 14:01:02 +0300

    da

--- src/engine/platform/sound/es5503.h
+++ src/engine/platform/sound/es5503.h
@@ -28,8 +28,8 @@ public:
 	void halt_osc(int onum, int type, uint32_t *accumulator, int resshift);
 	void fill_audio_buffer(short** buf, size_t len);
 
-	//unsigned char* sampleMem;
-	unsigned char sampleMem[65536 * 2];
+	unsigned char* sampleMem;
+	//unsigned char sampleMem[65536 * 2];
     size_t sampleMemLen;
 
 	uint32_t clock;
