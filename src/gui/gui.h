d61a8c8d9180ba796b3ae03bd98db350c3e77e41
freq-mod <konrad.curylo@proton.me>
2023-12-22 21:55:22 +0100

    tutorialr

--- src/gui/gui.h
+++ src/gui/gui.h
@@ -1174,23 +1174,6 @@ struct FurnaceGUISysCategory {
     description(NULL) {}
 };
 
-typedef std::function<void()> TutorialFunc;
-
-struct FurnaceGUITutorialStep {
-  const char* text;
-  int waitForTrigger;
-  TutorialFunc run;
-  TutorialFunc runBefore;
-  TutorialFunc runAfter;
-  
-  FurnaceGUITutorialStep(const char* t, int trigger=-1, TutorialFunc activeFunc=NULL, TutorialFunc beginFunc=NULL, TutorialFunc endFunc=NULL):
-    text(t),
-    waitForTrigger(trigger),
-    run(activeFunc),
-    runBefore(beginFunc),
-    runAfter(endFunc) {}
-};
-
 struct FurnaceGUIMacroDesc {
   //DivInstrumentMacro* macro;
   DivInstrument* ins;
