3f92cc801322f2b2a1d79319b12f828ffc19d6de
LTVA1 <87536432+LTVA1@users.noreply.github.com>
2023-12-22 23:08:20 +0300

    correct bug in OpenMPT paste

--- src/gui/editing.cpp
+++ src/gui/editing.cpp
@@ -771,10 +771,10 @@ unsigned int convertEffectMPT_S3M(unsigned char symbol, unsigned int val) {
           return (0x80<<8)|((val&0xf)<<4);
           break;
         case 0xC:
-          return (0xFC<<8)|(val&0xf);
+          return (0xEC<<8)|(val&0xf);
           break;
         case 0xD:
-          return (0xFD<<8)|(val&0xf);
+          return (0xED<<8)|(val&0xf);
           break;
         default:
           break;
