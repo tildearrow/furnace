e2a0a39569bfcd816ccc2efa4133705324f7c468
LTVA1 <87536432+LTVA1@users.noreply.github.com>
2024-01-09 11:55:07 +0300

    the fix

--- src/engine/platform/genesis.cpp
+++ src/engine/platform/genesis.cpp
@@ -351,9 +351,16 @@ void DivPlatformGenesis::tick(bool sysTick) {
     if (i>=5 && chan[i].furnaceDac) {
       if (NEW_ARP_STRAT) {
         chan[i].handleArp();
-      } else if (chan[i].std.get_div_macro_struct(DIV_MACRO_ARP)->had) {
+      } else if (chan[i].std.get_div_macro_struct(DIV_MACRO_ARP)->had && chan[i].dacMode) {
         if (!chan[i].inPorta) {
           chan[i].baseFreq=parent->calcBaseFreq(1,1,parent->calcArp(chan[i].note,chan[i].std.get_div_macro_struct(DIV_MACRO_ARP)->val),false);
+        }
+        chan[i].freqChanged=true;
+      }
+      else //when Ch6 is not in DAC mode
+      {
+        if (!chan[i].inPorta) {
+          chan[i].baseFreq=NOTE_FNUM_BLOCK(parent->calcArp(chan[i].note,chan[i].std.get_div_macro_struct(DIV_MACRO_ARP)->val),11);
         }
         chan[i].freqChanged=true;
       }
