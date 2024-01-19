6440b8c8085cc29729a38f0930882e8d46c37a86
cam900 <dbtlrchl@naver.com>
2024-01-19 23:13:10 +0900

    Add warning for MSM6295 maximum bankswitched sample length

--- src/gui/sampleEdit.cpp
+++ src/gui/sampleEdit.cpp
@@ -376,6 +376,9 @@ void FurnaceGUI::drawSampleEdit() {
             if (sample->loop) {
               SAMPLE_WARN(warnLoop,"MSM6295: samples can't loop");
             }
+            if (sample->samples>64511) {
+              SAMPLE_WARN(warnLength,"MSM6295: maximum bankswitched sample length is 64511");
+            }
           default:
             break;
         }
