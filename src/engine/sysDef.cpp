d35ac87ff66a4cfa8bb07ad44fb979b6e6d62947
tildearrow <me@tildearrow.org>
2024-03-22 02:21:33 -0500

    NDS: rename

--- src/engine/sysDef.cpp
+++ src/engine/sysDef.cpp
@@ -2056,7 +2056,7 @@ void DivEngine::registerSystems() {
   );
 
   sysDefs[DIV_SYSTEM_NDS]=new DivSysDef(
-    "NDS", NULL, 0xd6, 0, 16, false, true, 0, false, (1U<<DIV_SAMPLE_DEPTH_8BIT)|(1U<<DIV_SAMPLE_DEPTH_IMA_ADPCM)|(1U<<DIV_SAMPLE_DEPTH_16BIT), 32, 32,
+    "Nintendo DS", NULL, 0xd6, 0, 16, false, true, 0, false, (1U<<DIV_SAMPLE_DEPTH_8BIT)|(1U<<DIV_SAMPLE_DEPTH_IMA_ADPCM)|(1U<<DIV_SAMPLE_DEPTH_16BIT), 32, 32,
     "a handheld video game console with two screens. it uses a stylus.",
     {"Channel 1", "Channel 2", "Channel 3", "Channel 4", "Channel 5", "Channel 6", "Channel 7", "Channel 8", "Channel 9", "Channel 10", "Channel 11", "Channel 12", "Channel 13", "Channel 14", "Channel 15", "Channel 16"},
     {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16"},
