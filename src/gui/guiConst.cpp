1047d53ad4591de440353aad2e67dadc109671ee
Rockyfan75000 <153872220+Rockyfan75000@users.noreply.github.com>
2024-04-14 11:11:00 -0500

    Rework MSM5205

--- src/gui/guiConst.cpp
+++ src/gui/guiConst.cpp
@@ -1000,6 +1000,7 @@ const FurnaceGUIColorDef guiColors[GUI_COLOR_MAX]={
   D(GUI_COLOR_INSTR_NDS,"",ImVec4(0.7f,0.7f,0.8f,1.0f)),
   D(GUI_COLOR_INSTR_GBA_DMA,"",ImVec4(0.6f,0.4f,1.0f,1.0f)),
   D(GUI_COLOR_INSTR_GBA_MINMOD,"",ImVec4(0.5f,0.45f,0.7f,1.0f)),
+  D(GUI_COLOR_INSTR_MSM5205,"",ImVec4(1.0f,0.5f,0.7f,1.0f)),
   D(GUI_COLOR_INSTR_UNKNOWN,"",ImVec4(0.3f,0.3f,0.3f,1.0f)),
 
   D(GUI_COLOR_CHANNEL_BG,"",ImVec4(0.4f,0.6f,0.8f,1.0f)),
