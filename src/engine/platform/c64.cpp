9ba123953e6e839189da47ea97af2a8f3c01ac34
tildearrow <me@tildearrow.org>
2024-04-24 13:01:02 -0500

    C64: fix duty overflow
    just in case

--- src/engine/platform/c64.cpp
+++ src/engine/platform/c64.cpp
@@ -182,6 +182,7 @@ void DivPlatformC64::tick(bool sysTick) {
           chan[i].duty-=chan[i].std.duty.val;
         }
       }
+      chan[i].duty&=4095;
       rWrite(i*7+2,chan[i].duty&0xff);
       rWrite(i*7+3,chan[i].duty>>8);
     }
