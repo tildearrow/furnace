fde8b04192ab3703e383e9e310ed4f59b1b9b1c8
Brad Smith <brad@comstyle.com>
2024-05-05 18:22:22 -0400

    Fix build on OpenBSD
    IGFD appears to be a fork but I pushed this upstream as well.
    https://github.com/aiekick/ImGuiFileDialog/commit/30a00cf727b62c90fb667eefad06080786ca4616

--- extern/igfd/ImGuiFileDialog.cpp
+++ extern/igfd/ImGuiFileDialog.cpp
@@ -54,7 +54,7 @@ SOFTWARE.
   #ifndef PATH_MAX
     #define PATH_MAX 260
   #endif // PATH_MAX
-#elif defined(__linux__) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__APPLE__) || defined (__EMSCRIPTEN__) || defined(__HAIKU__)
+#elif defined(__linux__) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__APPLE__) || defined (__EMSCRIPTEN__) || defined(__HAIKU__)
   #define UNIX
   #define stricmp strcasecmp
   #include <sys/types.h>
