d16e940c4dd87ab0b48ac290839ea9859ec4dbd9
tildearrow <me@tildearrow.org>
2024-05-06 16:40:19 -0500

    fall back to software if no matching GL pixel form
    at available

--- src/gui/gui.cpp
+++ src/gui/gui.cpp
@@ -6987,7 +6987,14 @@ bool FurnaceGUI::init() {
   logD("creating window...");
   sdlWin=SDL_CreateWindow("Furnace",scrX,scrY,scrW,scrH,SDL_WINDOW_RESIZABLE|SDL_WINDOW_ALLOW_HIGHDPI|(scrMax?SDL_WINDOW_MAXIMIZED:0)|(fullScreen?SDL_WINDOW_FULLSCREEN_DESKTOP:0)|rend->getWindowFlags());
   if (sdlWin==NULL) {
-    lastError=fmt::sprintf("could not open window! %s",SDL_GetError());
+    const char* sdlErr=SDL_GetError();
+    lastError=fmt::sprintf("could not open window! %s",sdlErr);
+    if (settings.renderBackend!="Software" && strcmp(sdlErr,"No matching GL pixel format available")==0) {
+      settings.renderBackend="Software";
+      e->setConf("renderBackend","Software");
+      e->saveConf();
+      lastError+="\r\nfalling back to software renderer. please restart Furnace.";
+    }
     return false;
   }
 
