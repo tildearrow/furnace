a0e62fb00490fe41e6ca7237ec5336fac5a202e5
host12prog <hungnguyen.481335@gmail.com>
2024-08-06 21:26:07 +0700

    what a load of part 3

--- src/engine/platform/ay.cpp
+++ src/engine/platform/ay.cpp
@@ -155,10 +155,25 @@ void DivPlatformAY8910::runDAC() {
 }
 
 void DivPlatformAY8910::runTFX() {
+  /*
+  developer's note: if you are checking for intellivision
+  make sure to add "&& selCore"
+  because for some reason, the register remap doesn't work
+  when the user uses AtomicSSG core
+  */
   int timerPeriod, output;
   for (int i=0; i<3; i++) {
     if (chan[i].active && (chan[i].curPSGMode.val&16) && !(chan[i].curPSGMode.val&8) && chan[i].tfx.mode!=-1) {
       if (chan[i].tfx.mode == -1 && !isMuted[i]) {
+        /*
+        bug: if in the timer FX macro the user enables
+        and then disables PWM while there is no volume macro
+        there is now a random chance that the resulting output
+        is silent or has volume set incorrectly
+        i've tried to implement a fix, but it seems to be
+        ineffective, so...
+        TODO: actually implement a proper fix
+        */
         if (intellivision && chan[i].curPSGMode.getEnvelope()) {
           immWrite(0x08+i,(chan[i].outVol&0xc)<<2);
           continue;
@@ -172,12 +187,13 @@ void DivPlatformAY8910::runTFX() {
         chan[i].tfx.counter = 0;
         chan[i].tfx.out ^= 1;
         output = ((chan[i].tfx.out) ? chan[i].outVol : (chan[i].tfx.lowBound-(15-chan[i].outVol)));
-        // please stop crackling
+        // TODO: fix this stupid crackling noise that happens
+        // everytime the volume changes
         output = (output <= 0) ? 0 : output; // underflow
         output = (output >= 15) ? 15 : output; // overflow
         output &= 15; // i don't know if i need this but i'm too scared to remove it
         if (!isMuted[i]) {
-          if (intellivision) {
+          if (intellivision && selCore) {
             immWrite(0x0b+i,(output&0xc)<<2);
           } else {
             immWrite(0x08+i,output|(chan[i].curPSGMode.getEnvelope()<<2));
@@ -188,7 +204,6 @@ void DivPlatformAY8910::runTFX() {
         chan[i].tfx.counter = 0;
         if (!isMuted[i]) {
           if (intellivision && selCore) {
-            // it doesn't remap this in AtomicSSG for some reason
             immWrite(0xa, ayEnvMode);
           } else {
             immWrite(0xd, ayEnvMode);
@@ -205,6 +220,9 @@ void DivPlatformAY8910::runTFX() {
       timerPeriod = chan[i].freq*chan[i].tfx.den;
     }
     if (chan[i].tfx.num > 0 && chan[i].tfx.den > 0) chan[i].tfx.period=timerPeriod+chan[i].tfx.offset;
+    // stupid pitch correction because:
+    // YM2149 half-clock and Sunsoft 5B: timers run an octave too high
+    // on AtomicSSG core timers run 2 octaves too high
     if (clockSel || sunsoft) chan[i].tfx.period	= chan[i].tfx.period * 2;
     if (selCore) chan[i].tfx.period = chan[i].tfx.period * 4;
   }
@@ -744,7 +762,7 @@ int DivPlatformAY8910::dispatch(DivCommand c) {
     case DIV_CMD_STD_NOISE_MODE:
       if (c.value&0xf0 && !(chan[c.chan].nextPSGMode.val&8)) {
         chan[c.chan].nextPSGMode.val|=16;
-        chan[c.chan].tfx.mode = (c.value&3);
+        chan[i].tfx.mode=(c.value&3);
       }
       if (!(chan[c.chan].nextPSGMode.val&8)) {
         if (c.value<16) {
