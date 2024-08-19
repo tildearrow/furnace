fdf7b719446cf567314ee2b8395cd56fa79b8638
tildearrow <me@tildearrow.org>
2024-08-19 02:49:14 -0500

    Revert "don't run a delta against cached instrument if not insEditOpen"
    This reverts commit 2ab5ac4e075efe7dedc99c6ff7ea4d3d1de2542d.

--- src/gui/insEdit.cpp
+++ src/gui/insEdit.cpp
@@ -7747,7 +7747,7 @@ void FurnaceGUI::drawInsEdit() {
 }
 
 void FurnaceGUI::checkRecordInstrumentUndoStep() {
-  if (insEditOpen && curIns>=0 && curIns<(int)e->song.ins.size()) {
+  if (curIns>=0 && curIns<(int)e->song.ins.size()) {
     DivInstrument* ins=e->song.ins[curIns];
 
     // invalidate cachedCurIns/any possible changes if the cachedCurIns was referencing a different
