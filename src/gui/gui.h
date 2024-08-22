03961ed680245bea879f4b77481a6f8cfc0e167f
Adam Lederer <adam@adamlederer.com>
2024-08-22 02:43:30 -0700

    GUI_ACTION_PAT_ABSORB_INSTRUMENT search through prior orders (also set instrument to none if no instrument found

--- src/gui/gui.h
+++ src/gui/gui.h
@@ -2919,6 +2919,7 @@ class FurnaceGUI {
   void doExpand(int multiplier, const SelectionPoint& sStart, const SelectionPoint& sEnd);
   void doCollapseSong(int divider);
   void doExpandSong(int multiplier);
+  void doAbsorbInstrument();
   void doUndo();
   void doRedo();
   void doFind();
