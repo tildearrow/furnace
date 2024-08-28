87b172015ce02efa1a8d5a503ccb6a4e4c54caed
tildearrow <me@tildearrow.org>
2024-08-28 17:07:05 -0500

    fix 601x-604x

--- src/engine/platform/ym2608.cpp
+++ src/engine/platform/ym2608.cpp
@@ -1539,7 +1539,20 @@ int DivPlatformYM2608::dispatch(DivCommand c) {
     }
     case DIV_CMD_FM_OPMASK:
       if (c.chan>=psgChanOffs) break;
-      chan[c.chan].opMask=c.value&15;
+      switch (c.value>>4) {
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+          chan[c.chan].opMask&=~(1<<((c.value>>4)-1));
+          if (c.value&15) {
+            chan[c.chan].opMask|=(1<<((c.value>>4)-1));
+          }
+          break;
+        default:
+          chan[c.chan].opMask=c.value&15;
+          break;
+      }
       if (chan[c.chan].active) {
         chan[c.chan].opMaskChanged=true;
       }
