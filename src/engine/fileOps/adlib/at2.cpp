acb962828a552b27db6325c6a9d33ee8a972ddff
LTVA1 <87536432+LTVA1@users.noreply.github.com>
2024-09-10 18:30:51 +0300

    at2 convert slide to continuous

--- src/engine/fileOps/adlib/at2.cpp
+++ src/engine/fileOps/adlib/at2.cpp
@@ -2552,6 +2552,7 @@ bool DivEngine::loadAT2(unsigned char* file, size_t len)
             int vib_speed = 0;
             int trem_speed = 0;
             int vol_slide_speed = 0;
+            int slide_speed = 0;
 
             bool porta[2] = { false };
             bool vib[2] = { false };
@@ -2559,6 +2560,7 @@ bool DivEngine::loadAT2(unsigned char* file, size_t len)
             bool fine_porta[2] = { false };
             bool vol_slide[2] = { false };
             bool fine_vol_slide[2] = { false };
+            bool slide[2] = { false };
             
             for(int p = 0; p < s->ordersLen; p++)
             {
@@ -2577,6 +2579,7 @@ bool DivEngine::loadAT2(unsigned char* file, size_t len)
                     fine_porta[0] = false;
                     vol_slide[0] = false;
                     fine_vol_slide[0] = false;
+                    slide[0] = false;
 
                     bool found_fine_porta = false;
                     bool has_fine_porta = false;
@@ -2617,6 +2620,18 @@ bool DivEngine::loadAT2(unsigned char* file, size_t len)
                             }
 
                             vib_speed = param;
+                        }
+                        if(effect == 0x03)
+                        {
+                            slide[0] = true;
+
+                            if(slide_speed == param)
+                            {
+                                row_data[4 + eff * 2] = -1;
+                                row_data[5 + eff * 2] = -1; //delete effect
+                            }
+
+                            slide_speed = param;
                         }
                         if(effect == MARK_VOL_SLIDE || effect == 0x0A)
                         {
@@ -2724,6 +2739,14 @@ bool DivEngine::loadAT2(unsigned char* file, size_t len)
                         row_data[5 + emptyEffSlot * 2] = 0;
                     }
 
+                    if(!slide[0] && slide[1])
+                    {
+                        int emptyEffSlot = findEmptyEffectSlot(row_data);
+
+                        row_data[4 + emptyEffSlot * 2] = 0x03;
+                        row_data[5 + emptyEffSlot * 2] = 0;
+                    }
+
                     row_data[4 + (DIV_MAX_EFFECTS - 1) * 2] = -1; //erase continuous effects mark
 
                     porta_dir[1] = porta_dir[0];
@@ -2737,6 +2760,7 @@ bool DivEngine::loadAT2(unsigned char* file, size_t len)
                     fine_porta[1] = fine_porta[0];
                     vol_slide[1] = vol_slide[0];
                     fine_vol_slide[1] = fine_vol_slide[0];
+                    slide[1] = slide[0];
 
                     for(int s_ch = 0; s_ch < songInfo->nm_tracks; s_ch++) //search for 0Dxx/0Bxx and jump accordingly
                     {
