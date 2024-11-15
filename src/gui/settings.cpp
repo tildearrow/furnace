bbcca96384d3670dabdb47f8ade54d4ccfc80883
Eknous-P <ehovakimyan053@gmail.com>
2024-11-15 17:23:57 +0400

    settings: better cheats reveal nonLatchNibble redundant?

--- src/gui/settings.cpp
+++ src/gui/settings.cpp
@@ -3879,65 +3879,6 @@ void FurnaceGUI::setupSettingsCategories() {
           if (settings.backupMaxCopies>100) settings.backupMaxCopies=100;
         }
       })
-    }),
-    SettingsCategory(_("Cheat Codes"),{},{
-      // ok, so you decided to read the code.
-      // these are the cheat codes:
-      // "Debug" - toggles mobile UI
-      // "Nice Amiga cover of the song!" - enables hidden systems (YMU759/Dummy)
-      // "42 63" - enables all instrument types
-      // "4-bit FDS" - enables partial pitch linearity option
-      // "Power of the Chip" - enables options for multi-threaded audio (isnt that already enabled by default? maybe remove?)
-      // "btcdbcb" - use modern UI padding
-      // 
-      // "????" - enables stuff
-      SETTING(_("Cheat Codes"),{
-        ImGui::PushFont(headFont);
-        ImGui::Text(_("Enter code:"));
-        ImGui::PopFont();
-        ImGui::InputText("##CheatCode",&mmlString[31]);
-        if (ImGui::Button(_("Submit"))) {
-          unsigned int checker=0x11111111;
-          unsigned int checker1=0;
-          int index=0;
-          mmlString[30]=_("invalid code");
-
-          for (char& i: mmlString[31]) {
-            checker^=((unsigned int)i)<<index;
-            checker1+=i;
-            checker=(checker>>1|(((checker)^(checker>>2)^(checker>>3)^(checker>>5))&1)<<31);
-            checker1<<=1;
-            index=(index+1)&31;
-          }
-          if (checker==0x90888b65 && checker1==0x1482) {
-            mmlString[30]=_("toggled alternate UI");
-            toggleMobileUI(!mobileUI);
-          }
-          if (checker==0x5a42a113 && checker1==0xe4ef451e) {
-            mmlString[30]=_(":smile: :star_struck: :sunglasses: :ok_hand:");
-            settings.hiddenSystems=!settings.hiddenSystems;
-          }
-          if (checker==0xe888896b && checker1==0xbde) {
-            mmlString[30]=_("enabled all instrument types");
-            settings.displayAllInsTypes=!settings.displayAllInsTypes;
-          }
-          if (checker==0x3f88abcc && checker1==0xf4a6) {
-            mmlString[30]=_("OK, if I bring your Partial pitch linearity will you stop bothering me?");
-            settings.displayPartial=1;
-          }
-          if (checker==0x94222d83 && checker1==0x6600) {
-            mmlString[30]=_("enabled \"comfortable\" mode");
-            ImGuiStyle& sty=ImGui::GetStyle();
-            sty.FramePadding=ImVec2(20.0f*dpiScale,20.0f*dpiScale);
-            sty.ItemSpacing=ImVec2(10.0f*dpiScale,10.0f*dpiScale);
-            sty.ItemInnerSpacing=ImVec2(10.0f*dpiScale,10.0f*dpiScale);
-            settingsOpen=false;
-          }
-
-          mmlString[31]="";
-        }
-        ImGui::Text("%s",mmlString[30].c_str());
-      })
     })
   };
 
@@ -3961,9 +3902,6 @@ void FurnaceGUI::destroySettingsCategories(SettingsCategory& cat) {
 }
 
 void FurnaceGUI::drawSettingsCategory(SettingsCategory* cat) {
-  if (!nonLatchNibble) {
-    if (strncmp(cat->name, _("Cheat Codes"), 12)==0) return;
-  }
   if (cat->children.size()>0) {
     ImGuiTreeNodeFlags f=ImGuiTreeNodeFlags_SpanFullWidth|ImGuiTreeNodeFlags_OpenOnArrow|ImGuiTreeNodeFlags_OpenOnDoubleClick;
     if (settings.activeCategory.name==cat->name) f|=ImGuiTreeNodeFlags_Selected;
