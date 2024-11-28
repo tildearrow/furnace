11c0ea042855d9fcc715cec75ddfb2dcbd50b0bf
Eknous-P <ehovakimyan053@gmail.com>
2024-11-29 00:19:09 +0400

    autoscroll orders when overflow scolling patterns

--- src/gui/orders.cpp
+++ src/gui/orders.cpp
@@ -265,7 +265,7 @@ void FurnaceGUI::drawOrders() {
       bool tooSmall=((displayChans+1)>((ImGui::GetContentRegionAvail().x)/(ImGui::CalcTextSize("AA").x+2.0*ImGui::GetStyle().ItemInnerSpacing.x)));
       float yHeight=ImGui::GetContentRegionAvail().y;
       float lineHeight=(ImGui::GetTextLineHeight()+4*dpiScale);
-      if (e->isPlaying()) {
+      if (e->isPlaying() || haveHitBounds) {
         if (followOrders) {
           float nextOrdScroll=(playOrder+1)*lineHeight-((yHeight-(tooSmall?ImGui::GetStyle().ScrollbarSize:0.0f))/2.0f);
           if (nextOrdScroll<0.0f) nextOrdScroll=0.0f;
