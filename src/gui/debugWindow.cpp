e539db8058fbff6d7effc0004ea2c2469f77dac1
tildearrow <me@tildearrow.org>
2024-12-13 18:59:38 -0500

    it compiles

--- src/gui/debugWindow.cpp
+++ src/gui/debugWindow.cpp
@@ -26,6 +26,8 @@
 #include "imgui.h"
 #include "imgui_internal.h"
 
+#include <lua.hpp>
+
 PendingDrawOsc _debugDo;
 static float oscDebugData[2048];
 static int oscDebugLen=800;
@@ -738,6 +740,9 @@ void FurnaceGUI::drawDebug() {
       ImGui::InputInt("Chan",&getGainChan);
       ImGui::InputInt("Vol",&getGainVol);
       ImGui::Text("result: %.0f%%",realVol*100.0f);
+      ImGui::TreePop();
+    }
+    if (ImGui::TreeNode("Script Test")) {
       ImGui::TreePop();
     }
     if (ImGui::TreeNode("Cursor Undo Debug")) {
