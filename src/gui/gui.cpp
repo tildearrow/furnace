a7572266adfee30cc2a564e68597d899a0c567b2
recme <121174166+recolorme@users.noreply.github.com>
2025-04-14 19:42:34 -0400

    Update gui.cpp

--- src/gui/gui.cpp
+++ src/gui/gui.cpp
@@ -1186,6 +1186,7 @@ void FurnaceGUI::play(int row) {
   memset(chanOscBright,0,DIV_MAX_CHANS*sizeof(float));
   e->walkSong(loopOrder,loopRow,loopEnd);
   memset(lastIns,-1,sizeof(int)*DIV_MAX_CHANS);
+  if (wasFollowing=true) followPattern = true;
   if (followPattern) makeCursorUndo();
   if (!followPattern) e->setOrder(curOrder);
   if (row>=0) {
