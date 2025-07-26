98030de8c708a24ad9dc1b50f8fa1fa076121cc5
tildearrow <me@tildearrow.org>
2025-07-26 18:48:23 -0500

    dev233 - breaking the limit, part 1
    now up to 32768 wavetables and 32768 samples
    
    this is the first part and does not implement the new sample limit correctly
    I have to adapt every dispatch to the new limit... see you in the next part
    
    the format version had to be bumped because the WL and SL .fui features were limited to 256 entries
    there are new LW and LS blocks with more space
    howwver there's a new issue... we can have a feature larger than 65536, which is a limit imposed by the feature header :<
    this will be addressed though

--- src/engine/engine.h
+++ src/engine/engine.h
@@ -54,8 +54,8 @@ class DivWorkPool;
 
 #define DIV_UNSTABLE
 
-#define DIV_VERSION "dev232"
-#define DIV_ENGINE_VERSION 232
+#define DIV_VERSION "dev233"
+#define DIV_ENGINE_VERSION 233
 // for imports
 #define DIV_VERSION_MOD 0xff01
 #define DIV_VERSION_FC 0xff02
