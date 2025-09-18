0548e88f47889b0723b9a2c10024cb308b1d64c4
tildearrow <me@tildearrow.org>
2025-09-18 05:11:56 -0500

    prepare for a new ImGui file picker

---
+++ src/gui/newFilePicker.h
@@ -0,0 +1,37 @@
+/**
+ * Furnace Tracker - multi-system chiptune tracker
+ * Copyright (C) 2021-2025 tildearrow and contributors
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include "../ta-utils.h"
+
+class FurnaceFilePicker {
+  struct FileEntry {
+    String path;
+    String name;
+    uint64_t size;
+    struct tm time;
+  };
+  std::vector<FileEntry*> entries;
+  std::vector<FileEntry*> sortedEntries;
+  std::mutex entryLock;
+  String path;
+
+  public:
+    bool draw();
+    bool open(String name);
+};
