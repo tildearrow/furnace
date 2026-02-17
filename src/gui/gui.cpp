447eb803834685a98f8c57acd93fba1607a720d6
Tomasz Sterna <tomasz@sterna.link>
2026-02-17 23:17:17 +0100

    [SGU] SGU FM WavePreview

--- src/gui/gui.cpp
+++ src/gui/gui.cpp
@@ -9508,6 +9508,8 @@ FurnaceGUI::FurnaceGUI():
   fmPreviewOPLL(NULL),
   fmPreviewOPZ(NULL),
   fmPreviewOPZInterface(NULL),
+  fmPreviewESFM(NULL),
+  fmPreviewSGU(NULL),
   editString(NULL),
   pendingRawSampleDepth(8),
   pendingRawSampleChannels(1),
