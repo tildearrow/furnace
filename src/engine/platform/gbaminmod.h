273faead19279940cf7d17b9fb5b440ebab346f4
tildearrow <me@tildearrow.org>
2026-05-16 05:17:26 -0500

    GBA MinMod: use DivPitchTable WARNING
    the generated table has extremely high values.
    it overflows on low rates!!!!!!
    this must be fixed somehow

--- src/engine/platform/gbaminmod.h
+++ src/engine/platform/gbaminmod.h
@@ -111,6 +111,7 @@ class DivPlatformGBAMinMod: public DivDispatch {
     void notifyInsChange(int ins);
     void notifyWaveChange(int wave);
     void notifyInsDeletion(void* ins);
+    void notifyPitchTable(int sample=-1);
     void poke(unsigned int addr, unsigned short val);
     void poke(std::vector<DivRegWrite>& wlist);
     const char** getRegisterSheet();
